e27c287a3eaeaea362fbe8a532a66de6836edf19
kushal19057 <kushal19057@iiitd.ac.in>
2022-11-07 00:35:14 +0530

    remove nonsense stuff

--- cloud_project/account/views.py
+++ cloud_project/account/views.py
@@ -7,7 +7,6 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .tokens import account_activation_token
 
@@ -18,20 +17,6 @@ def dashboard(request):
     return render(request,
                   'account/user/dashboard.html',
                   {'section': 'profile', 'orders': orders})
-
-
-@login_required
-def edit_details(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-
-        if user_form.is_valid():
-            user_form.save()
-    else:
-        user_form = UserEditForm(instance=request.user)
-
-    return render(request,
-                  'account/user/edit_details.html', {'user_form': user_form})
 
 
 @login_required
