c7ef3c1b6cd57a715e0ae7bf67db9b486ec75e52
kushal19057 <kushal19057@iiitd.ac.in>
2022-11-07 00:09:04 +0530

    bring in ideas from fcs project

--- cloud_project/account/admin.py
+++ cloud_project/account/admin.py
@@ -1,39 +1,5 @@
 from django.contrib import admin
-from django.db.models import Q
-from django.http import HttpRequest
 
-from .models import DakuUser
+from .models import UserBase
 
-
-# Register your models here.
-@admin.register(DakuUser)
-class BuyerAdmin(admin.ModelAdmin):
-
-    def has_delete_permission(self, request: HttpRequest, obj = None) -> bool:
-        if obj is None:
-            return True
-        else:
-            return not obj.is_superuser
-
-    def has_add_permission(self, request: HttpRequest) -> bool:
-        return False
-
-    def has_change_permission(self, request: HttpRequest, obj=None) -> bool:
-        return False
-
-    def get_queryset(self, request):
-        return self.model.objects.filter(Q(is_active=True))
-
-    ordering = ('-created',)
-    list_display = (
-        'user_name', 'email',
-    )
-
-    readonly_fields = (
-        'user_name',
-        'email',
-    )
-
-    fieldsets = (
-        ('Credentials', {'fields': ('user_name', 'email',)}),
-    )
+admin.site.register(UserBase)
--- cloud_project/account/forms.py
+++ cloud_project/account/forms.py
@@ -1,7 +1,9 @@
-
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
+                                       SetPasswordForm)
+
 from .models import UserBase
+
 
 class UserLoginForm(AuthenticationForm):
 
@@ -15,24 +17,95 @@ class UserLoginForm(AuthenticationForm):
         }
     ))
 
-class RegistrationForm(UserCreationForm):
-    user_name = forms.CharField(min_length=8, max_length=16)
-    email = forms.EmailField(max_length=50)
+
+class RegistrationForm(forms.ModelForm):
+
+    user_name = forms.CharField(
+        label='Enter Username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required', error_messages={
+        'required': 'Sorry, you will need an email'})
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = UserBase
-        fields = ('user_name', 'email', 'password1', 'password2')
+        fields = ('user_name', 'email',)
 
-    def clean_username(self):
-        user_name = self.cleaned_data['user_name'].lower().strip()
+    def clean_user_name(self):
+        user_name = self.cleaned_data['user_name'].lower()
         r = UserBase.objects.filter(user_name=user_name)
         if r.count():
-            raise forms.ValidationError('Please use another username, that is already taken')
+            raise forms.ValidationError("Username already exists")
         return user_name
 
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match.')
+        return cd['password2']
+
     def clean_email(self):
-        email = self.cleaned_data['email'].strip()
-        r = UserBase.objects.filter(email=email)
-        if r.count():
-            raise forms.ValidationError('Please use another Email, that is already taken')
+        email = self.cleaned_data['email']
+        if UserBase.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                'Please use another Email, that is already taken')
         return email
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_name'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_email'})
+        self.fields['password'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Repeat Password'})
+
+
+class PwdResetForm(PasswordResetForm):
+
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = UserBase.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError(
+                'Unfortunatley we can not find that email address')
+        return email
+
+
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+    new_password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
+
+
+class UserEditForm(forms.ModelForm):
+
+    email = forms.EmailField(
+        label='Account email (can not be changed)', max_length=200, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email', 'readonly': 'readonly'}))
+
+    user_name = forms.CharField(
+        label='Firstname', min_length=4, max_length=50, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-firstname', 'readonly': 'readonly'}))
+
+    first_name = forms.CharField(
+        label='Username', min_length=4, max_length=50, widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Firstname', 'id': 'form-lastname'}))
+
+    class Meta:
+        model = UserBase
+        fields = ('email', 'user_name', 'first_name',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_name'].required = True
+        self.fields['email'].required = True
