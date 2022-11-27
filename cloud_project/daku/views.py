e27562ce8f006aa786f83d2f546150dd9ec2d3fd
kushal19057 <kushal19057@iiitd.ac.in>
2022-11-27 11:59:06 +0530

    got upload file to work locally

--- cloud_project/daku/views.py
+++ cloud_project/daku/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
-from .functions import get_docker_ip_port, upload_file_to_container, run_beast_on_container
+from .functions import get_docker_ip_port
 import json
 
 @login_required
@@ -11,22 +11,10 @@ def daku_home(request):
 
 @login_required
 def daku_file_upload(request):
-    # handle POST request
-    if request.method == "POST":
-        # upload_file_to_container(request.user, request.body['upload_file'])
-        data = {"message": "SUCCESS",}
-        return JsonResponse(data)
-    else:
-        # handle GET request
-        return render(request, 'daku/file_upload.html')
+    ip, port = get_docker_ip_port(request.user)
+    return render(request, 'daku/file_upload.html', {"docker_ip": ip, "docker_port": port})
     
 @login_required
 def daku_file_editor(request):
-    # handle POST request
-    if request.method == "POST":
-        run_beast_on_container(request.user, request.FILES)
-        data = {"message": "SUCCESS"}
-        return JsonResponse(data)
-    else:
-        # handle GET request
-        return render(request, "daku/beast_run.html")
+    ip, port = get_docker_ip_port(request.user)
+    return render(request, "daku/beast_run.html", {"docker_ip": ip, "docker_port": port})
