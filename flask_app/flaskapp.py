5c9504cb27802430b432e052cd951d3fce5337da
kushal19057 <kushal19057@iiitd.ac.in>
2022-12-02 19:25:14 +0530

    added flask db models

--- flask_app/flaskapp.py
+++ flask_app/flaskapp.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '1f1e1210f56638c8d6536435c6f19b5a'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    container_id = db.Column(db.Integer, db.ForeignKey('container.id'), nullable=False)
+    plan = db.Column(db.String(20), nullable=False, default="BASIC")
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Container(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    container_id = db.Column(db.String(120), unique=True, nullable=False)
+    time_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user = db.relationship('User', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f"Container('{self.container_id}', '{self.user}')"
+
 
 @app.route("/")
 @app.route("/home")
