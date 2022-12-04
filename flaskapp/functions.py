e82f7f55c8b67ae5fb83f885416dc7df6ca17db3
UDIT NARANG <udit19120@iiitd.ac.in>
2022-12-04 16:25:34 +0530

    added if condition

--- flaskapp/functions.py
+++ flaskapp/functions.py
@@ -4,4 +4,6 @@ def get_docker_ip_port(user):
     client = docker.from_env()
     container = client.containers.get(user.container_id)
     ip, port = container.ports['8080/tcp'][0]['HostIp'], container.ports['8080/tcp'][0]['HostPort']
+    if ip=="0.0.0.0":
+        ip="localhost"
     return ip, port
