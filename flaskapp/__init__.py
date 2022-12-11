fed66131f33dedf60bd54e44072669426a0171de
UDIT NARANG <udit19120@iiitd.ac.in>
2022-12-11 15:02:57 +0530

    updated logic for server selection

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import redis
 
 app = Flask(__name__)
 
@@ -15,6 +16,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+r = redis.Redis(host='redis-18253.c264.ap-south-1-1.ec2.cloud.redislabs.com', port=18253, db=0, password='oc01To5gRD86hOcEoBD4RMFdpLvk8Ikz')
 
 from flaskapp import routes
 
--- flaskapp/functions.py
+++ flaskapp/functions.py
@@ -1,6 +1,11 @@
 import docker
 import json
 import random
+from flaskapp import r
+
+
+inf=int(1e9)
+
 
 def get_docker_ip_port(user):
     # ip, port = container.ports['8080/tcp'][0]['HostIp'], container.ports['8080/tcp'][0]['HostPort']
@@ -11,4 +16,20 @@ def get_container_ip()->str:
         file = json.load(f)
 
     ips = file["server_ips"]
-    return random.choice(ips)
+
+    dict_load={}
+
+    for ip in ips:
+        dict_load[ip]=inf
+    
+
+    for ip in ips:
+        cpu_percent=float(r.get(f"{ip}-cpu_percent"))
+        virtual_memory=float(r.get(f"{ip}-virtual_memory_percent"))
+        dict_load[ip]=cpu_percent*virtual_memory
+
+
+    dict_load=dict(sorted(dict_load.items(), key=lambda item: item[1]))
+
+    print(list(dict_load.keys())[0])
+    return str(list(dict_load.keys())[0])
