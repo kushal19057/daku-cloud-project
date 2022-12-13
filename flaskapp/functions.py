82107950620a40d58e0d46411f0eb6c03713ab9c
kushal19057 <kushal19057@iiitd.ac.in>
2022-12-13 21:10:23 +0530

    revert back to original logic to allocate servers

--- flaskapp/functions.py
+++ flaskapp/functions.py
@@ -17,19 +17,4 @@ def get_container_ip()->str:
 
     ips = file["server_ips"]
 
-    dict_load={}
-
-    for ip in ips:
-        dict_load[ip]=inf
-    
-
-    for ip in ips:
-        cpu_percent=float(r.get(f"{ip}-cpu_percent"))
-        virtual_memory=float(r.get(f"{ip}-virtual_memory_percent"))
-        dict_load[ip]=cpu_percent*virtual_memory
-
-
-    dict_load=dict(sorted(dict_load.items(), key=lambda item: item[1]))
-
-    print(list(dict_load.keys())[0])
-    return str(list(dict_load.keys())[0])
+    return random.choice(ips)
